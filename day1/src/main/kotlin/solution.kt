321c1194a46780ff237ed3725bc93bd774d615fc
Mon Zafra <monzee@gmail.com>
2016-12-01 20:37:44 +0800

    day1: changed impl to sealed class

--- day1/src/main/kotlin/solution.kt
+++ day1/src/main/kotlin/solution.kt
@@ -2,34 +2,41 @@ fun main(vararg args: String) {
     readLine()?.let { println(solve(it)) }
 }
 
-fun solve(map: String): Int = map.split(", ").fold(Turtle()) { curr, step ->
-    val dir = step.first()
-    val disp = step.drop(1).toInt()
-    when (dir.toUpperCase()) {
-        'L' -> curr.left(disp)
-        'R' -> curr.right(disp)
-        else -> throw IllegalArgumentException("direction must be L or R")
-    }
-}.distance
+fun solve(moves: String): Int {
+    return moves.split(", ").fold(Cursor.Up(0, 0) as Cursor) { current, move ->
+        val dir = move.first()
+        val disp = move.drop(1).toInt()
+        when (dir.toUpperCase()) {
+            'L' -> current.left(disp)
+            'R' -> current.right(disp)
+            else -> throw IllegalArgumentException("direction must be L or R")
+        }
+    }.distance
+}
 
-enum class Dir { UP, DOWN, LEFT, RIGHT }
-
-class Turtle(val x: Int, val y: Int, val dir: Dir) {
-    constructor(): this(0, 0, Dir.UP)
-
+sealed class Cursor(val x: Int, val y: Int) {
     val distance: Int by lazy { Math.abs(x) + Math.abs(y) }
 
-    fun left(disp: Int): Turtle = when (dir) {
-        Dir.UP -> Turtle(x - disp, y, Dir.LEFT)
-        Dir.DOWN -> Turtle(x + disp, y, Dir.RIGHT)
-        Dir.LEFT -> Turtle(x,  y - disp, Dir.DOWN)
-        Dir.RIGHT -> Turtle(x, y + disp, Dir.UP)
+    abstract fun left(disp: Int): Cursor
+    abstract fun right(disp: Int): Cursor
+
+    class Up(x: Int, y: Int) : Cursor(x, y) {
+        override fun left(disp: Int) = Left(x - disp, y)
+        override fun right(disp: Int) = Right(x + disp, y)
     }
 
-    fun right(disp: Int): Turtle = when (dir) {
-        Dir.UP -> Turtle(x + disp, y, Dir.RIGHT)
-        Dir.DOWN -> Turtle(x - disp, y, Dir.LEFT)
-        Dir.LEFT -> Turtle(x,  y + disp, Dir.UP)
-        Dir.RIGHT -> Turtle(x, y - disp, Dir.DOWN)
+    class Down(x: Int, y: Int) : Cursor(x, y) {
+        override fun left(disp: Int) = Right(x + disp, y)
+        override fun right(disp: Int) = Left(x - disp, y)
+    }
+
+    class Left(x: Int, y: Int) : Cursor(x, y) {
+        override fun left(disp: Int) = Down(x, y - disp)
+        override fun right(disp: Int) = Up(x, y + disp)
+    }
+
+    class Right(x: Int, y: Int) : Cursor(x, y) {
+        override fun left(disp: Int) = Up(x, y + disp)
+        override fun right(disp: Int) = Down(x, y - disp)
     }
 }
