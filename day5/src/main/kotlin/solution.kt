d628d712483f4e214e6d7c00d0343496a8b1c415
Mon Zafra <monzee@gmail.com>
2016-12-05 23:19:36 +0800

    day5: refactored both parts to work off a single seq

--- day5/src/main/kotlin/solution.kt
+++ day5/src/main/kotlin/solution.kt
@@ -1,70 +1,48 @@
 package advent.day5
 
 import java.security.MessageDigest
-import java.util.BitSet
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.ArrayBlockingQueue
 
 fun main(vararg args: String) {
     readLine()?.let {
-        when {
-            "--hard-mode" in args -> solve(it)
-            else -> simple(it)
-        }
+        val simpleScheme = ArrayBlockingQueue<Char>(8)
+        solve(it, simpleScheme)
+        println(simpleScheme.joinToString(""))
     }
 }
 
-fun solve(prefix: String): String = generateSequence(0) { it + 1 }
+fun solve(
+        prefix: String,
+        aside: BlockingQueue<Char>? = null
+): String = generateSequence(0, Int::inc)
         .map { "$prefix$it" }
-        .map(::hash)
-        .mapNotNull(::nextCharInPosition)
-        .fold(Password()) { password, found ->
-            val (i, char) = found
-            password[i] = char
-            password.apply {
-                if (complete) {
-                    return chars.toString()
-                }
-            }
+        .map(::md5)
+        .filter { it.startsWith("00000") }
+        .onEach {
+            aside?.apply { offer(it[5]) }
         }
-        .let { "unreachable!" }
-
-fun simple(prefix: String): String = generateSequence(0) { it + 1 }
-        .map { "$prefix$it" }
-        .map(::hash)
-        .mapNotNull(::nextChar)
+        .filter { it[5] in '0'..'7' }
+        .distinctBy { it[5] }
         .take(8)
-        .map(::print)
-        .joinToString("")
+        .fold(StringBuilder("_".repeat(8))) { password, hash ->
+            password[hash[5] - '0'] = hash[6]
+            password.apply(::println)
+        }
+        .toString()
 
 private val MD5 = MessageDigest.getInstance("MD5")
 private val BYTE_STR = (-128..127).map { "%02x".format(it.toByte()) }
 
-fun hash(text: String): CharSequence = StringBuilder().apply {
+fun md5(text: String): CharSequence = StringBuilder().apply {
     text.toByteArray()
             .let { MD5.digest(it) }
             .forEach { append(BYTE_STR[it + 128]) }
 }
 
-fun nextChar(hash: CharSequence): Char? = when {
-    hash.startsWith("00000") -> hash[5]
-    else -> null
-}
-
-data class Password(val size: Int = 8) {
-    val chars = StringBuilder("_".repeat(size))
-    val complete: Boolean get() = flags.cardinality() == size
-    private val flags = BitSet(size)
-
-    operator fun set(i: Int, char: Char) {
-        require(i in 0 until size) { "out of bounds" }
-        if (!flags.get(i)) {
-            flags.set(i)
-            chars[i] = char
-            println(chars)
-        }
-    }
-}
-
-fun nextCharInPosition(hash: CharSequence): Pair<Int, Char>? = when {
-    !hash.startsWith("00000") || hash[5] !in '0'..'7' -> null
-    else -> (hash[5] - '0') to hash[6]
+inline fun <T> Sequence<T>.onEach(
+        crossinline action: (T) -> Unit
+): Sequence<T> = map {
+    action(it)
+    it
 }
--- day5/src/test/kotlin/tests.kt
+++ day5/src/test/kotlin/tests.kt
@@ -2,36 +2,45 @@ package advent.day5
 
 import kotlin.test.*
 import org.junit.Test
+import java.util.concurrent.ArrayBlockingQueue
 
 class Md5Test {
     @Test
     fun starts_with_five_zeroes() {
-        assertEquals("00000", hash("abc3231929").take(5))
-        assertEquals("00000", hash("abc5017308").take(5))
+        assertEquals("00000", md5("abc3231929").take(5))
+        assertEquals("00000", md5("abc5017308").take(5))
     }
 
     @Test
     fun next_char() {
-        assertEquals('1', hash("abc3231929").let(::nextChar))
-        assertEquals('8', hash("abc5017308").let(::nextChar))
-        assertNull(hash("whatever").let(::nextChar))
+        assertEquals('1', md5("abc3231929").let(::nextChar))
+        assertEquals('8', md5("abc5017308").let(::nextChar))
+        assertNull(md5("whatever").let(::nextChar))
     }
 
     @Test
     fun positioned_char() {
-        assertEquals(1 to '5', hash("abc3231929").let(::nextCharInPosition))
-        assertNull(hash("abc5017308").let(::nextCharInPosition))
-        assertEquals(4 to 'e', hash("abc5357525").let(::nextCharInPosition))
+        assertEquals(1 to '5', md5("abc3231929").let(::nextCharInPosition))
+        assertNull(md5("abc5017308").let(::nextCharInPosition))
+        assertEquals(4 to 'e', md5("abc5357525").let(::nextCharInPosition))
     }
 }
 
 class Solution {
     @Test
     fun example() {
-        todo {
-            // takes forever to finish but it's correct
-             assertEquals("18f47a30", simple("abc"))
-        }
-        assertEquals("05ace8e3", solve("abc"))
+        val simple = ArrayBlockingQueue<Char>(8)
+        assertEquals("05ace8e3", solve("abc", simple))
+        assertEquals("18f47a30", simple.joinToString(""))
     }
+}
+
+fun nextChar(hash: CharSequence): Char? = when {
+    hash.startsWith("00000") -> hash[5]
+    else -> null
+}
+
+fun nextCharInPosition(hash: CharSequence): Pair<Int, Char>? = when {
+    hash[5] in '0'..'7' -> (hash[5] - '0') to hash[6]
+    else -> null
 }
