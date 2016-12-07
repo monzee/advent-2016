b8b9efa273301292bfa20dad25e88ad810fc0217
Mon Zafra <monzee@gmail.com>
2016-12-07 16:40:29 +0800

    day7: solved

--- day7/src/kotlin/solution.kt
+++ day7/src/kotlin/solution.kt
@@ -9,34 +9,51 @@ import org.junit.Assume.*
 import org.hamcrest.CoreMatchers.*
 
 fun main(vararg args: String) {
-    generateSequence(::readLine)
-            .count(::hasPalindrome)
-            .echo("part 1")
+    generateSequence(::readLine).toList().let {
+        it.count(::supportsTls).echo("part 1")
+        it.count(::supportsSsl).echo("part 2")
+    }
 }
 
 fun solve(vararg parts: () -> Any): List<Any> = parts.map { it() }
 
 val SPEC = Regex("""
-        [^\[\]]{4,}  \]|  # enclosed segment
-        [^\[\]]{4,}  \[?  # outer
+        [^\[\]]{3,}  \]|  # enclosed segment
+        [^\[\]]{3,}  \[?  # outer
         """.trimIndent(), RegexOption.COMMENTS)
 
-fun hasPalindrome(entry: String): Boolean = SPEC
-        .findAll(entry)
+fun split(address: String): Pair<List<String>, List<String>> = SPEC
+        .findAll(address)
         .map { it.groups.first()!!.value }
         .partition { it.endsWith(']') }
-        .let {
-            val (inner, outer) = it
-            inner.all { !hasAbba(it) }.apply{if(!this) echo(entry.error)} && outer.any(::hasAbba)
-        }
-        .apply{if(this) echo(entry.debug)}
+
+fun supportsTls(entry: String): Boolean = split(entry).let {
+    val (subnets, supernets) = it
+    subnets.all { !hasAbba(it) } && supernets.any(::hasAbba)
+}
 
 fun hasAbba(segment: String): Boolean = (0..segment.lastIndex - 3)
         .map { segment.substring(it, it + 4) }
         .any(::isAbba)
 
-fun isAbba(it: CharSequence): Boolean =
+fun isAbba(it: String): Boolean =
         it[0] != it[1] && it[1] == it[2] && it[0] == it[3]
+
+fun supportsSsl(entry: String): Boolean = split(entry).let {
+    val (subnets, supernets) = it.echo
+    supernets.flatMap(::abasOf)
+            .any { 
+                babOf(it).let { bab -> subnets.any { bab in it.echo(bab)!! } }
+            }
+}
+
+fun abasOf(segment: String): List<String> = (0..segment.lastIndex - 2)
+        .map { segment.substring(it, it + 3) }
+        .filter(::isAba)
+
+fun isAba(it: String): Boolean = it[0] != it[1] && it[0] == it[2]
+
+fun babOf(aba: String): String = charArrayOf(aba[1], aba[0], aba[1]).joinToString("")
 
 // === test area === //
 
@@ -58,10 +75,18 @@ class scratch {
     }
 
     @Test
-    fun has_external_palindrome() {
-        assertTrue(hasPalindrome("abba[mnop]qrst"))
-        assertFalse(hasPalindrome("abcd[bddb]xyyx"))
-        assertFalse(hasPalindrome("aaaa[qwer]tyui"))
-        assertTrue(hasPalindrome("ioxxoj[asdfgh]zxcvbn"))
+    fun part1_examples() {
+        assertTrue(supportsTls("abba[mnop]qrst"))
+        assertFalse(supportsTls("abcd[bddb]xyyx"))
+        assertFalse(supportsTls("aaaa[qwer]tyui"))
+        assertTrue(supportsTls("ioxxoj[asdfgh]zxcvbn"))
+    }
+
+    @Test
+    fun part2_examples() {
+        assertTrue(supportsSsl("aba[bab]xyz"))
+        assertFalse(supportsSsl("xyx[xyx]xyx"))
+        assertTrue(supportsSsl("aaa[kek]eke"))
+        assertTrue(supportsSsl("zazbz[bzb]cdb"))
     }
 }
